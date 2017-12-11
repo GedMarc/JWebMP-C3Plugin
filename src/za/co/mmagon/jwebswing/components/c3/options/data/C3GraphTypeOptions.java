2b82986e2b0306e9910ffc95005acd07b2cc32fc
Marc Magon <ged_marc@hotmail.com>
2017-12-11 09:10:21 +0200

    More String Deduplication and Bootstrap Card 4b2 Update

--- src/za/co/mmagon/jwebswing/components/c3/options/data/C3GraphTypeOptions.java
+++ src/za/co/mmagon/jwebswing/components/c3/options/data/C3GraphTypeOptions.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright (C) 2017 Marc Magon
  *
  * This program is free software: you can redistribute it and/or modify
@@ -17,6 +17,8 @@
 package za.co.mmagon.jwebswing.components.c3.options.data;
 
 import za.co.mmagon.jwebswing.utilities.StaticStrings;
+
+import static za.co.mmagon.jwebswing.utilities.StaticStrings.CHAR_DASH;
 
 /**
  * @author GedMarc
@@ -40,6 +42,6 @@ public enum C3GraphTypeOptions
 	@Override
 	public String toString()
 	{
-		return name().replace(StaticStrings.CHAR_UNDERSCORE, '-').toLowerCase();
+		return name().replace(StaticStrings.CHAR_UNDERSCORE, CHAR_DASH).toLowerCase();
 	}
 }
