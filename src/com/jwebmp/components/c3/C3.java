5039f03c7a648244740007549ccfa327b2ac7592
Marc Magon <ged_marc@hotmail.com>
2018-04-14 08:28:38 +0200

    Prepping for RC1 - CRP Finalizations Slim Scroll

---
+++ src/com/jwebmp/components/c3/C3.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (C) 2017 Marc Magon
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.jwebmp.components.c3;
+
+import com.jwebmp.base.html.Div;
+import com.jwebmp.plugins.ComponentInformation;
+
+import java.util.Objects;
+
+/**
+ * An implementation of the C3 Graphs project.
+ * <p>
+ *
+ * @author Marc Magon
+ * @version 1.0
+ * @since 29 Aug 2015
+ */
+@ComponentInformation(name = "C3 Graphing",
+		description = "C3 makes it easy to generate D3-based charts by wrapping the code required to construct the entire chart. We don't need to write D3 code any more. ",
+		url = "https://www.c3js.org",
+		wikiUrl = "https://github.com/GedMarc/JWebSwing-C3Plugin/wiki")
+public class C3
+		extends Div<C3Children, C3Attributes, C3Features, C3Events, C3>
+{
+
+	private static final long serialVersionUID = 1L;
+	private C3Feature feature;
+
+	/**
+	 * Constructs a new C3 Graph
+	 */
+	public C3()
+	{
+		addFeature(getFeature());
+	}
+
+	public final C3Feature getFeature()
+	{
+		if (feature == null)
+		{
+			feature = new C3Feature(this);
+		}
+		return feature;
+	}
+
+	@Override
+	public C3Options getOptions()
+	{
+		return getFeature().getOptions();
+	}
+
+	@Override
+	public boolean equals(Object o)
+	{
+		if (this == o)
+		{
+			return true;
+		}
+		if (!(o instanceof C3))
+		{
+			return false;
+		}
+		if (!super.equals(o))
+		{
+			return false;
+		}
+		C3 c3 = (C3) o;
+		return Objects.equals(getFeature(), c3.getFeature());
+	}
+
+	@Override
+	public int hashCode()
+	{
+		return Objects.hash(super.hashCode(), getFeature());
+	}
+}
---
+++ src/com/jwebmp/components/c3/options/data/C3RowData.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright (C) 2017 Marc Magon
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.jwebmp.components.c3.options.data;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Objects;
+
+/**
+ * An ArrayList of Specific Row Data.
+ * Is an ArrayList of type Object
+ *
+ * @author GedMarc
+ * @since 09 Mar 2016
+ */
+public class C3RowData<D> extends ArrayList<D> implements Comparable<C3RowData>, Comparator<C3RowData>
+{
+
+	private static final long serialVersionUID = 1L;
+	private C3RowDataType type;
+
+	public C3RowData()
+	{
+		//Nothing Needed
+	}
+
+	@Override
+	public int compareTo(C3RowData o)
+	{
+		if (this.type == C3RowDataType.Header)
+		{
+			return -1;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+
+	@Override
+	public int compare(C3RowData o1, C3RowData o2)
+	{
+		if (o1 != null && o2 != null)
+		{
+			if (o1.getType() == C3RowDataType.Header)
+			{
+				return -1;
+			}
+			else
+			{
+				return 0;
+			}
+
+		}
+
+		return 0;
+	}
+
+	/**
+	 * Returns the type for the C3RowData
+	 *
+	 * @return
+	 */
+	public C3RowDataType getType()
+	{
+		return type;
+	}
+
+	/**
+	 * Sets the type for the C3Row Data
+	 *
+	 * @param type
+	 */
+	public void setType(C3RowDataType type)
+	{
+		this.type = type;
+	}
+
+	@Override
+	public boolean equals(Object o)
+	{
+		if (this == o)
+		{
+			return true;
+		}
+		if (!(o instanceof C3RowData))
+		{
+			return false;
+		}
+		if (!super.equals(o))
+		{
+			return false;
+		}
+		C3RowData<?> c3RowData = (C3RowData<?>) o;
+		return getType() == c3RowData.getType();
+	}
+
+	@Override
+	public int hashCode()
+	{
+		return Objects.hash(super.hashCode(), getType());
+	}
+}
