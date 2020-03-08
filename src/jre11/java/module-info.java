8d3b6023c47e318402cdbd03b78bb7f0703a3104
GedMarc <ged_marc@hotmial.com>
2020-03-08 17:00:25 +0200

    Remove Jar syntax as JDK 8 is no longer supported

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -12,7 +12,6 @@ module com.jwebmp.plugins.c3 {
 	requires com.guicedee.guicedinjection;
 
 	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.c3.C3PageConfigurator;
-	provides com.guicedee.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.c3.implementations.C3ExclusionsModule;
 	provides com.guicedee.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.c3.implementations.C3ExclusionsModule;
 
 	opens com.jwebmp.plugins.c3 to com.fasterxml.jackson.databind, com.jwebmp.core;
--- src/main/java/com/jwebmp/plugins/c3/implementations/C3ExclusionsModule.java
+++ src/main/java/com/jwebmp/plugins/c3/implementations/C3ExclusionsModule.java
@@ -1,6 +1,5 @@
 package com.jwebmp.plugins.c3.implementations;
 
-import com.guicedee.guicedinjection.interfaces.IGuiceScanJarExclusions;
 import com.guicedee.guicedinjection.interfaces.IGuiceScanModuleExclusions;
 
 import javax.validation.constraints.NotNull;
@@ -8,18 +7,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class C3ExclusionsModule
-		implements IGuiceScanModuleExclusions<C3ExclusionsModule>,
-				           IGuiceScanJarExclusions<C3ExclusionsModule>
+		implements IGuiceScanModuleExclusions<C3ExclusionsModule>
 {
-
-	@Override
-	public @NotNull Set<String> excludeJars()
-	{
-		Set<String> strings = new HashSet<>();
-		strings.add("jwebmp-c3-*");
-		return strings;
-	}
-
 	@Override
 	public @NotNull Set<String> excludeModules()
 	{
